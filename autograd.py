1cb25f2372fb4664e127b328a25fc47e45b0588b
Naren Sengodan <scartegy@gmail.com>
2026-08-14 00:49:26 +0530

    INFO : Autograd gradient

---
+++ autograd.py
@@ -0,0 +1,61 @@
+class Value:
+
+    def __init__(self, data, _children=(), _op=""):
+        self.data = data
+        self.grad = 0.0
+        self._prev = set(_children)
+        self._op = _op
+        self._backward = lambda : None
+
+    def __add__(self, other):
+
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self, other), "*")
+
+        def _backward():
+            self.grad  += out.grad * 1
+            other.grad += out.grad * 1
+
+        out._backward = _backward
+        return out
+
+    def __mul__(self, other):
+
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self, other), "*")
+
+        def _backward():
+            self.grad += out.grad * other.data
+            other.grad += out.grad * self.data
+
+        out._backward = _backward
+        return out
+
+    def backward(self):
+        topo = []
+        visited = set()
+
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for parent in v._prev:
+                    build_topo(parent)
+                topo.append(v)
+
+        build_topo(self)
+
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
+
+
+a = Value(2.0)
+b = Value(-3.0)
+c = a + b
+d = a * b
+e = c * d
+
+e.backward()
+
+print(a.grad)  # should be -3.0
+print(b.grad)  # should be -8.0
